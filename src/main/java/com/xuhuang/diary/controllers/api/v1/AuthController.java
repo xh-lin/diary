6690ae980e96466b5592e5fbe88e7171910cc1a8
Xu Huang Lin <XuHuang.Lin001@umb.edu>
2022-02-16 16:11:21 -0500

    Logout API

--- src/main/java/com/xuhuang/diary/controllers/api/v1/AuthController.java
+++ src/main/java/com/xuhuang/diary/controllers/api/v1/AuthController.java
@@ -1,5 +1,8 @@
 package com.xuhuang.diary.controllers.api.v1;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import com.xuhuang.diary.domains.AuthRequest;
 import com.xuhuang.diary.services.UserService;
 
@@ -7,8 +10,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.*;
 
 import lombok.RequiredArgsConstructor;
@@ -42,6 +47,16 @@ public class AuthController {
         }
 
         return new ResponseEntity<>("Logged-in successfully", HttpStatus.OK);
+    }
+
+    @PostMapping("logout")
+    public ResponseEntity<String> fetchSignoutSite(HttpServletRequest request, HttpServletResponse response) {        
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null) {
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+          
+        return new ResponseEntity<>("Logged-out successfully", HttpStatus.OK);
     }
     
 }
