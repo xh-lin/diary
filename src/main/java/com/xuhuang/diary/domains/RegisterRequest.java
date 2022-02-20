a04de1ff816b4292d6b606f32d2801099122c5d2
Xu Huang Lin <XuHuang.Lin001@umb.edu>
2022-02-20 17:31:49 -0500

    Add validations to RegisterRequest

--- src/main/java/com/xuhuang/diary/domains/RegisterRequest.java
+++ src/main/java/com/xuhuang/diary/domains/RegisterRequest.java
@@ -1,5 +1,9 @@
 package com.xuhuang.diary.domains;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,8 +14,38 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class RegisterRequest {
+
+    @Pattern(
+        regexp = "^[a-zA-Z0-9_-]*$", 
+        message = "Username must contain only hyphens (-), underscores (_), letters or numbers.")
+    @Pattern(
+        regexp = "^.*[a-zA-Z0-9].*$", 
+        message = "Username must contain at least one letter or number.")
+    @Size(
+        min = 4, max = 16,
+        message = "Username length must be between {min} and {max}.")
     private String username;
+
+    @Email(message = "Email format not valid.")
     private String email;
+
+    @Pattern(
+        regexp = "^.*[a-z].*$", 
+        message = "Password must contain at least one lowercase letter.")
+    @Pattern(
+        regexp = "^.*[A-Z].*$", 
+        message = "Password must contain at least one uppercase letter.")
+    @Pattern(
+        regexp = "^.*[0-9].*$", 
+        message = "Password must contain at least one number.")
+    @Pattern(
+        regexp = "^.*[^\\w\\s].*$", 
+        message = "Password must contain at least one special character.")
+    @Size(
+        min = 8, max = 32,
+        message = "Password length must be between {min} and {max}.")
     private String password;
+
     private String passwordConfirm;
+
 }
