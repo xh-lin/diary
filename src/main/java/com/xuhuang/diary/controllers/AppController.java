7b3bb5dd4d076503a4709c4a888c8946747e225e
Xu Huang Lin <XuHuang.Lin001@umb.edu>
2022-02-21 14:25:00 -0500

    Add validation to registration page

--- src/main/java/com/xuhuang/diary/controllers/AppController.java
+++ src/main/java/com/xuhuang/diary/controllers/AppController.java
@@ -1,12 +1,19 @@
 package com.xuhuang.diary.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import javax.security.auth.message.AuthException;
+import javax.validation.Valid;
 
 import com.xuhuang.diary.domains.RegisterRequest;
 import com.xuhuang.diary.services.UserService;
 
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -31,12 +38,26 @@ public class AppController {
     }
 
     @PostMapping("/register")
-    public String submitRegistration(RegisterRequest request, Model model) {
+    public String submitRegistration(@Valid RegisterRequest request, BindingResult br, Model model) {
+        if (br.hasErrors()) {
+            List<String> errors = br.getAllErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.toList());
+
+            model.addAttribute("request", request);
+            model.addAttribute("errors", errors);
+            return "register";
+        }
+
         try {
             userService.register(request);
         } catch (AuthException e) {
+            List<String> errors = new ArrayList<>();
+            errors.add(e.getMessage());
+
             model.addAttribute("request", request);
-            model.addAttribute("errorMsg", e.getMessage());
+            model.addAttribute("errors", errors);
             return "register";
         }
         
@@ -47,4 +68,5 @@ public class AppController {
     public String viewLoginPage() {
         return "login";
     }
+
 }
