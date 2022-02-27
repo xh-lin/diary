6ff9e325c32f9fb22ec8fa10a680325331c0dd88
Xu Huang Lin <XuHuang.Lin001@umb.edu>
2022-02-27 15:25:45 -0500

    App test appController, authRestController, and userService not null

--- src/test/java/com/xuhuang/diary/DiaryApplicationTests.java
+++ src/test/java/com/xuhuang/diary/DiaryApplicationTests.java
@@ -1,13 +1,29 @@
 package com.xuhuang.diary;
 
+import com.xuhuang.diary.controllers.AppController;
+import com.xuhuang.diary.controllers.api.v1.AuthRestController;
+import com.xuhuang.diary.services.UserService;
+
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DiaryApplicationTests {
 
+	@Autowired
+	private AppController appController;
+	@Autowired
+	private AuthRestController authRestController;
+	@Autowired
+	private UserService userService;
+
 	@Test
 	void contextLoads() {
+		Assertions.assertThat(appController).isNotNull();
+		Assertions.assertThat(authRestController).isNotNull();
+		Assertions.assertThat(userService).isNotNull();
 	}
 
 }
