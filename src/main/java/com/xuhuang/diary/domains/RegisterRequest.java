52bbbe72529e8c07ba674472e0e00a4ae846bcea
Xu Huang Lin <XuHuang.Lin001@umb.edu>
2022-02-27 15:16:58 -0500

    Add NotBlank constraints for RegisterRequest fields

--- src/main/java/com/xuhuang/diary/domains/RegisterRequest.java
+++ src/main/java/com/xuhuang/diary/domains/RegisterRequest.java
@@ -1,6 +1,7 @@
 package com.xuhuang.diary.domains;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
@@ -15,25 +16,31 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@FieldMatch(fields = {"password", "passwordConfirm"}, message = RegisterRequest.PASSWORD_CONFIRMATION_DOES_NOT_MATCH)
+@FieldMatch(fields = {"password", "passwordConfirm"}, message = RegisterRequest.VALIDATION_MESSAGE_PASSWORD_CONFIRMATION)
 public class RegisterRequest {
 
-    public static final String PASSWORD_CONFIRMATION_DOES_NOT_MATCH = "Password confirmation does not match.";
+    public static final String VALIDATION_MESSAGE_PASSWORD_CONFIRMATION = "Password confirmation does not match.";
 
     public static final String VALIDATION_MESSAGE_USERNAME_CONTAIN_ONLY = "Username must contain only hyphens (-), underscores (_), letters or numbers.";
     public static final String VALIDATION_MESSAGE_USERNAME_AT_LEAST = "Username must contain at least one letter or number.";
     public static final String VALIDATION_MESSAGE_USERNAME_SIZE = "Username length must be between {min} and {max}.";
-    public static final String VALIDATION_MESSAGE_EMAIL = "Email format not valid.";
+    public static final String VALIDATION_MESSAGE_USERNAME_NOTBLANK = "Username must not be blank.";
     public static final int USERNAME_SIZE_MIN = 4;
     public static final int USERNAME_SIZE_MAX = 16;
+
+    public static final String VALIDATION_MESSAGE_EMAIL = "Email format not valid.";
+    public static final String VALIDATION_MESSAGE_EMAIL_NOTBLANK = "Email must not be blank.";
     
     public static final String VALIDATION_MESSAGE_PASSWORD_LOWER = "Password must contain at least one lowercase letter.";
     public static final String VALIDATION_MESSAGE_PASSWORD_UPPER = "Password must contain at least one uppercase letter.";
     public static final String VALIDATION_MESSAGE_PASSWORD_NUMBER = "Password must contain at least one number.";
     public static final String VALIDATION_MESSAGE_PASSWORD_SPECIAL = "Password must contain at least one special character.";
     public static final String VALIDATION_MESSAGE_PASSWORD_SIZE = "Password length must be between {min} and {max}.";
+    public static final String VALIDATION_MESSAGE_PASSWORD_NOTBLANK = "Password must not be blank.";
     public static final int PASSWORD_SIZE_MIN = 8;
     public static final int PASSWORD_SIZE_MAX = 32;
+
+    public static final String VALIDATION_MESSAGE_PASSWORD_CONFIRM_NOTBLANK = "Password confirmation must not be blank.";
 
     @Pattern(
         regexp = "^[a-zA-Z0-9_-]*$", 
@@ -44,9 +51,11 @@ public class RegisterRequest {
     @Size(
         min = USERNAME_SIZE_MIN, max = USERNAME_SIZE_MAX,
         message = VALIDATION_MESSAGE_USERNAME_SIZE)
+    @NotBlank(message = VALIDATION_MESSAGE_USERNAME_NOTBLANK)
     private String username;
 
     @Email(message = VALIDATION_MESSAGE_EMAIL)
+    @NotBlank(message = VALIDATION_MESSAGE_EMAIL_NOTBLANK)
     private String email;
 
     @Pattern(
@@ -64,8 +73,10 @@ public class RegisterRequest {
     @Size(
         min = PASSWORD_SIZE_MIN, max = PASSWORD_SIZE_MAX,
         message = VALIDATION_MESSAGE_PASSWORD_SIZE)
+    @NotBlank(message = VALIDATION_MESSAGE_PASSWORD_NOTBLANK)
     private String password;
 
+    @NotBlank(message = VALIDATION_MESSAGE_PASSWORD_CONFIRM_NOTBLANK)
     private String passwordConfirm;
 
 }
