af743effd120b6ae0f2c7f19d9ceff9452907546
Xu Huang Lin <XuHuang.Lin001@umb.edu>
2022-04-03 18:20:23 -0400

    Fix couldn't load css file before login

--- src/main/java/com/xuhuang/diary/security/WebSecurityConfig.java
+++ src/main/java/com/xuhuang/diary/security/WebSecurityConfig.java
@@ -26,7 +26,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
             .authorizeRequests()
-                .antMatchers("/api/v*/auth/*", "/register", "/error", "/error/*").permitAll()
+                .antMatchers("/api/v*/auth/*", "/register", "/error", "/error/*", "/css/global.css").permitAll()
                 .anyRequest().authenticated()
                 .and()
             .formLogin()
