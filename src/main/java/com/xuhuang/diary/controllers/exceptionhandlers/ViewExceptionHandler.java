ecc018e1c9812ed951eb3b30d00b0aaf50d825a3
Xu Huang Lin <XuHuang.Lin001@umb.edu>
2022-04-09 15:49:40 -0400

    Logging in ViewExceptionHandler

--- src/main/java/com/xuhuang/diary/controllers/exceptionhandlers/ViewExceptionHandler.java
+++ src/main/java/com/xuhuang/diary/controllers/exceptionhandlers/ViewExceptionHandler.java
@@ -8,6 +8,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @ControllerAdvice("com.xuhuang.diary.controllers.view")
 public class ViewExceptionHandler {
 
@@ -24,6 +27,8 @@ public class ViewExceptionHandler {
         } else {
             status = HttpStatus.INTERNAL_SERVER_ERROR;
         }
+
+        log.info("ex: {}, status: {}", ex, status);
 
         return String.format("redirect:/error/%d?message=%s", status.value(), ex.getMessage());
     }
