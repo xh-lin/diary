c342f1800e026331c36edadc9a57cf47d696d1f4
Xu Huang Lin <XuHuang.Lin001@umb.edu>
2022-04-09 12:59:22 -0400

    Rework error page and controller

--- src/main/java/com/xuhuang/diary/controllers/exceptionhandlers/ViewExceptionHandler.java
+++ src/main/java/com/xuhuang/diary/controllers/exceptionhandlers/ViewExceptionHandler.java
@@ -4,6 +4,7 @@ import java.util.NoSuchElementException;
 
 import javax.security.auth.message.AuthException;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -12,15 +13,19 @@ public class ViewExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public String handleException(Exception ex) {
-        if (ex instanceof IllegalArgumentException) {
-            return "redirect:/error/400";
-        } else if (ex instanceof AuthException) {
-            return "redirect:/error/403";
+        HttpStatus status;
+
+        if (ex instanceof AuthException) {
+            status = HttpStatus.FORBIDDEN;
         } else if (ex instanceof NoSuchElementException) {
-            return "redirect:/error/404";
+            status = HttpStatus.NOT_FOUND;
+        } else if (ex instanceof IllegalArgumentException) {
+            status = HttpStatus.BAD_REQUEST;
         } else {
-            return "redirect:/error/500";
+            status = HttpStatus.INTERNAL_SERVER_ERROR;
         }
+
+        return String.format("redirect:/error/%d?message=%s", status.value(), ex.getMessage());
     }
 
 }
--- src/main/java/com/xuhuang/diary/controllers/view/v1/ErrorController.java
+++ src/main/java/com/xuhuang/diary/controllers/view/v1/ErrorController.java
@@ -2,10 +2,13 @@ package com.xuhuang.diary.controllers.view.v1;
 
 import com.xuhuang.diary.controllers.view.Template;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import lombok.RequiredArgsConstructor;
 
@@ -14,43 +17,12 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ErrorController {
 
-    private static final String TITLE_400 = "400 Bad Request";
-    private static final String MESSAGE_400 = "Bad Request";
-    private static final String TITLE_403 = "403 Forbidden";
-    private static final String MESSAGE_403 = "Forbidden";
-    private static final String TITLE_404 = "404 Not Found";
-    private static final String MESSAGE_404 = "Not Found";
-    private static final String TITLE_500 = "500 Internal Server Error";
-    private static final String MESSAGE_500 = "Internal Server Error";
-
-    private static final String TITLE = "title";
-    private static final String CODE = "code";
-    private static final String MESSAGE = "message";
-
-    @GetMapping("/400")
-    public String view400(Model model) {
-        return viewError(model, TITLE_400, "400", MESSAGE_400);
-    }
-
-    @GetMapping("/403")
-    public String view403(Model model) {
-        return viewError(model, TITLE_403, "403", MESSAGE_403);
-    }
-
-    @GetMapping("/404")
-    public String view404(Model model) {
-        return viewError(model, TITLE_404, "404", MESSAGE_404);
-    }
-
-    @GetMapping("/500")
-    public String view500(Model model) {
-        return viewError(model, TITLE_500, "500", MESSAGE_500);
-    }
-
-    private String viewError(Model model, String title, String code, String message) {
-        model.addAttribute(TITLE, title);
-        model.addAttribute(CODE, code);
-        model.addAttribute(MESSAGE, message);
+    @GetMapping("/{statusCode}")
+    public String viewError(Model model, @PathVariable int statusCode, @RequestParam(required = false) String message) {
+        HttpStatus status = HttpStatus.valueOf(statusCode);
+        model.addAttribute("status", status.value());
+        model.addAttribute("error", status.getReasonPhrase());
+        model.addAttribute("message", message);
         return Template.ERROR.toString();
     }
 
