de73aa0a63d7283f33b619023b3c9656cb69ef41
Xu Huang Lin <XuHuang.Lin001@umb.edu>
2022-04-12 18:32:17 -0400

    Delete tag feature

--- src/main/resources/static/js/fragments/tag.js
+++ src/main/resources/static/js/fragments/tag.js
@@ -6,19 +6,42 @@
 console.assert(TAGS_FRAGMENT_URL !== undefined);
 
 const TAG_ID_PREFIX = '#tag_';
-const TAG_BUTTONS_ID = '#tagButtons';
 const TAG_DIALOG_MODAL_ID = '#tagDialogModal';
+const TAG_BUTTONS_ID = '#tagButtons';
 
 const CREATE_TAG_BUTTON_GROUP_ID = '#createTagButtonGroup';
 const CREATE_TAG_FORM_TOGGLE_BUTTON_ID = '#createTagFormToggleButton';
 const CREATE_TAG_FORM_ID = '#createTagForm';
 
+const DELETE_TAG_DIALOG_MODAL_ID = '#deleteTagDialogModal';
+const DELETE_TAG_FORM_ID = '#deleteTagForm';
+const DELETE_TAG_NAME_ID = '#deleteTagName';
+
 const TAG_DIALOG_MODAL_LABEL_ID = '#tagDialogModalLabel';
 const SELECT_TAG_LABEL_ID = '#selectTagLabel';
 const DELETE_TAG_LABEL_ID = '#deleteTagLabel';
 
-const TAG_ACTION_BUTTONS_ID = '#tagActionButtons';
 const TAG_CANCEL_ACTION_BUTTON_ID = '#tagCancelActionButton';
+const TAG_ACTION_BUTTONS_ID = '#tagActionButtons';
+const DELETE_TAG_BUTTON_ID = '#deleteTagButton';
+
+const tagDialogModal = $(TAG_DIALOG_MODAL_ID);
+const tagButtons = tagDialogModal.find(TAG_BUTTONS_ID);
+
+const createTagButtonGroup = $(CREATE_TAG_BUTTON_GROUP_ID);
+const createTagFormToggleButton = createTagButtonGroup.find(CREATE_TAG_FORM_TOGGLE_BUTTON_ID);
+const createTagForm = createTagButtonGroup.find(CREATE_TAG_FORM_ID);
+
+const deleteTagDialogModal = new bootstrap.Modal(DELETE_TAG_DIALOG_MODAL_ID);
+const deleteTagForm = $(DELETE_TAG_DIALOG_MODAL_ID).find(DELETE_TAG_FORM_ID);
+
+const tagDialogModalLabel = $(TAG_DIALOG_MODAL_LABEL_ID);
+const selectTagLabel = tagDialogModalLabel.find(SELECT_TAG_LABEL_ID);
+const deleteTagLabel = tagDialogModalLabel.find(DELETE_TAG_LABEL_ID);
+
+const tagCancelActionButton = $(TAG_CANCEL_ACTION_BUTTON_ID);
+const tagActionButtons = $(TAG_ACTION_BUTTONS_ID);
+const deleteTagButtons = tagActionButtons.find(DELETE_TAG_BUTTON_ID);
 
 // enum for tag action states
 const TagAction = {
@@ -27,11 +50,6 @@ const TagAction = {
 };
 Object.freeze(TagAction);
 let currentAction = TagAction.SELECT;
-
-const tagDialogModal = $(TAG_DIALOG_MODAL_ID);
-const tagButtons = $(TAG_BUTTONS_ID);
-const createTagFormToggleButton = $(CREATE_TAG_FORM_TOGGLE_BUTTON_ID);
-const createTagForm = $(CREATE_TAG_FORM_ID);
 
 /*
     Functions for toggling tag actions
@@ -51,15 +69,15 @@ function showSelectTagAction() {
     currentAction = TagAction.SELECT;
 
     // change dialog title
-    tagDialogModal.find(TAG_DIALOG_MODAL_LABEL_ID).children().hide()
-    tagDialogModal.find(SELECT_TAG_LABEL_ID).show();
+    tagDialogModalLabel.children().hide()
+    selectTagLabel.show();
 
     // change action button
-    tagDialogModal.find(TAG_CANCEL_ACTION_BUTTON_ID).hide();
-    tagDialogModal.find(TAG_ACTION_BUTTONS_ID).show();
+    tagCancelActionButton.hide();
+    tagActionButtons.show();
 
     // Show create tag action button
-    tagDialogModal.find(CREATE_TAG_BUTTON_GROUP_ID).show();
+    createTagButtonGroup.show();
     hideCreateTagForm();
 }
 
@@ -67,19 +85,19 @@ function showDeleteTagAction() {
     currentAction = TagAction.DELETE;
 
     // change dialog title
-    tagDialogModal.find(TAG_DIALOG_MODAL_LABEL_ID).children().hide()
-    tagDialogModal.find(DELETE_TAG_LABEL_ID).show();
+    tagDialogModalLabel.children().hide()
+    deleteTagLabel.show();
 
     // change action button
-    tagDialogModal.find(TAG_ACTION_BUTTONS_ID).hide();
-    tagDialogModal.find(TAG_CANCEL_ACTION_BUTTON_ID).show();
+    tagActionButtons.hide();
+    tagCancelActionButton.show();
 
     // Hide create tag action button
-    tagDialogModal.find(CREATE_TAG_BUTTON_GROUP_ID).hide();
+    createTagButtonGroup.hide();
 }
 
 /*
-    Click button to show create tag form
+    Toggle create tag form display button
 */
 
 createTagFormToggleButton.click(function () {
@@ -95,7 +113,7 @@ tagDialogModal.on('hidden.bs.modal', function () {
 });
 
 /*
-    create tag
+    Create tag form submit
 */
 
 setupAjaxFormSubmit(createTagForm, function (res) {
@@ -108,24 +126,65 @@ setupAjaxFormSubmit(createTagForm, function (res) {
         contentType: 'application/json',
         error: errorHandler,
         success: function (tagsFragment) {
+            const newTags = $(tagsFragment);
+            // setup on click handler
+            newTags.find('button').click(tagOnClickHandler);
             // append new tags fragment
-            tagButtons.append($(tagsFragment).children());
+            tagButtons.append(newTags.children());
         }
     });
 }, false);
 
 /*
-    Delete tag
+    Toggle delete tag action button
 */
 
-tagDialogModal.find('#deleteTagButton').click(function () {
+deleteTagButtons.click(function () {
     showDeleteTagAction();
 });
 
 /*
-    Button for canceling tag action
+    Cancel tag action button
 */
 
-tagDialogModal.find('#tagCancelActionButton').click(function () {
+tagCancelActionButton.click(function () {
     showSelectTagAction();
+});
+
+/*
+    Tag button on click handler
+*/
+
+function tagOnClickHandler(event) {
+    switch (currentAction) {
+        case TagAction.SELECT:
+            break;
+        case TagAction.DELETE:
+            const button = $(event.currentTarget);
+
+            // Extract info from data-bs-* attributes
+            const name = button.attr('data-bs-name');
+            const url = button.attr('data-bs-url');
+
+            // Update the modal's content.
+            deleteTagForm.attr('action', url);
+            deleteTagForm.find(DELETE_TAG_NAME_ID).text(name);
+
+            deleteTagDialogModal.show()
+            break;
+        default:
+            console.error(`Unhandled TagAction: ${currentAction}`);
+    }
+}
+
+tagButtons.find('button').click(tagOnClickHandler);
+
+/*
+    Delete tag form submit
+*/
+
+setupAjaxFormSubmit(deleteTagForm, function (res) {
+    const TAG_FRAGMENT_ID = TAG_ID_PREFIX + res.data.id;
+    const tagButton = tagButtons.find(TAG_FRAGMENT_ID);
+    tagButton.remove();
 });
