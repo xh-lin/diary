cbcfc8c2626ece383e8f55b31e7329564b2f281c
Xu Huang Lin <XuHuang.Lin001@umb.edu>
2022-04-12 18:42:17 -0400

    Organize tag.js

--- src/main/resources/static/js/fragments/tag.js
+++ src/main/resources/static/js/fragments/tag.js
@@ -10,7 +10,7 @@ const TAG_DIALOG_MODAL_ID = '#tagDialogModal';
 const TAG_BUTTONS_ID = '#tagButtons';
 
 const CREATE_TAG_BUTTON_GROUP_ID = '#createTagButtonGroup';
-const CREATE_TAG_FORM_TOGGLE_BUTTON_ID = '#createTagFormToggleButton';
+const CREATE_TAG_FORM_TOGGLE_BUTTON_ID = '#createTagFormDisplayButton';
 const CREATE_TAG_FORM_ID = '#createTagForm';
 
 const DELETE_TAG_DIALOG_MODAL_ID = '#deleteTagDialogModal';
@@ -29,7 +29,7 @@ const tagDialogModal = $(TAG_DIALOG_MODAL_ID);
 const tagButtons = tagDialogModal.find(TAG_BUTTONS_ID);
 
 const createTagButtonGroup = $(CREATE_TAG_BUTTON_GROUP_ID);
-const createTagFormToggleButton = createTagButtonGroup.find(CREATE_TAG_FORM_TOGGLE_BUTTON_ID);
+const createTagFormDisplayButton = createTagButtonGroup.find(CREATE_TAG_FORM_TOGGLE_BUTTON_ID);
 const createTagForm = createTagButtonGroup.find(CREATE_TAG_FORM_ID);
 
 const deleteTagDialogModal = new bootstrap.Modal(DELETE_TAG_DIALOG_MODAL_ID);
@@ -41,7 +41,7 @@ const deleteTagLabel = tagDialogModalLabel.find(DELETE_TAG_LABEL_ID);
 
 const tagCancelActionButton = $(TAG_CANCEL_ACTION_BUTTON_ID);
 const tagActionButtons = $(TAG_ACTION_BUTTONS_ID);
-const deleteTagButtons = tagActionButtons.find(DELETE_TAG_BUTTON_ID);
+const deleteTagButton = tagActionButtons.find(DELETE_TAG_BUTTON_ID);
 
 // enum for tag action states
 const TagAction = {
@@ -56,12 +56,12 @@ let currentAction = TagAction.SELECT;
 */
 
 function showCreateTagForm() {
-    createTagFormToggleButton.addClass('d-none');
+    createTagFormDisplayButton.addClass('d-none');
     createTagForm.removeClass('d-none')
 }
 
 function hideCreateTagForm() {
-    createTagFormToggleButton.removeClass('d-none')
+    createTagFormDisplayButton.removeClass('d-none')
     createTagForm.addClass('d-none');
 }
 
@@ -97,57 +97,22 @@ function showDeleteTagAction() {
 }
 
 /*
-    Toggle create tag form display button
+    Toggle tag actions
 */
 
-createTagFormToggleButton.click(function () {
+createTagFormDisplayButton.click(function () {
     showCreateTagForm();
 });
 
-/*
-    Reset action state after dialog closes
-*/
-
-tagDialogModal.on('hidden.bs.modal', function () {
-    showSelectTagAction();
-});
-
-/*
-    Create tag form submit
-*/
-
-setupAjaxFormSubmit(createTagForm, function (res) {
-    hideCreateTagForm();
-    // load tags fragment
-    $.ajax({
-        type: 'POST',
-        url: TAGS_FRAGMENT_URL,
-        data: JSON.stringify([res.data]),
-        contentType: 'application/json',
-        error: errorHandler,
-        success: function (tagsFragment) {
-            const newTags = $(tagsFragment);
-            // setup on click handler
-            newTags.find('button').click(tagOnClickHandler);
-            // append new tags fragment
-            tagButtons.append(newTags.children());
-        }
-    });
-}, false);
-
-/*
-    Toggle delete tag action button
-*/
-
-deleteTagButtons.click(function () {
+deleteTagButton.click(function () {
     showDeleteTagAction();
 });
 
-/*
-    Cancel tag action button
-*/
-
 tagCancelActionButton.click(function () {
+    showSelectTagAction();
+});
+
+tagDialogModal.on('hidden.bs.modal', function () {
     showSelectTagAction();
 });
 
@@ -178,6 +143,29 @@ function tagOnClickHandler(event) {
 }
 
 tagButtons.find('button').click(tagOnClickHandler);
+
+/*
+    Create tag form submit
+*/
+
+setupAjaxFormSubmit(createTagForm, function (res) {
+    hideCreateTagForm();
+    // load tags fragment
+    $.ajax({
+        type: 'POST',
+        url: TAGS_FRAGMENT_URL,
+        data: JSON.stringify([res.data]),
+        contentType: 'application/json',
+        error: errorHandler,
+        success: function (tagsFragment) {
+            const newTags = $(tagsFragment);
+            // setup on click handler
+            newTags.find('button').click(tagOnClickHandler);
+            // append new tags fragment
+            tagButtons.append(newTags.children());
+        }
+    });
+}, false);
 
 /*
     Delete tag form submit
