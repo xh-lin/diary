79f49ade6168fab4eb25ea80f29d8ecf2d5365db
Xu Huang Lin <XuHuang.Lin001@umb.edu>
2022-04-15 17:24:12 -0400

    Add size validation to login & register request bodies

--- src/main/java/com/xuhuang/diary/domains/RegisterRequest.java
+++ src/main/java/com/xuhuang/diary/domains/RegisterRequest.java
@@ -5,6 +5,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.xuhuang.diary.validators.FieldMatch;
 import com.xuhuang.diary.validators.UniqueEmail;
 import com.xuhuang.diary.validators.UniqueUsername;
@@ -29,11 +30,13 @@ public class RegisterRequest {
     public static final String USERNAME_SIZE = "Username length must be between {min} and {max}.";
     public static final String USERNAME_NOTBLANK = "Username must not be blank.";
     public static final int USERNAME_SIZE_MIN = 4;
-    public static final int USERNAME_SIZE_MAX = 16;
+    public static final int USERNAME_SIZE_MAX = 63;
 
     public static final String EMAIL_ALREADY_TAKEN = "Email already taken.";
     public static final String EMAIL_FORMAT = "Email format not valid.";
+    public static final String EMAIL_SIZE = "Email length must be less than or equal to {max}.";
     public static final String EMAIL_NOTBLANK = "Email must not be blank.";
+    public static final int EMAIL_SIZE_MAX = 255;
 
     public static final String PASSWORD_LOWER = "Password must contain at least one lowercase letter.";
     public static final String PASSWORD_UPPER = "Password must contain at least one uppercase letter.";
@@ -42,8 +45,9 @@ public class RegisterRequest {
     public static final String PASSWORD_SIZE = "Password length must be between {min} and {max}.";
     public static final String PASSWORD_NOTBLANK = "Password must not be blank.";
     public static final int PASSWORD_SIZE_MIN = 8;
-    public static final int PASSWORD_SIZE_MAX = 32;
+    public static final int PASSWORD_SIZE_MAX = 127;
 
+    public static final String PASSWORD_CONFIRM_SIZE = "Password confirmation length must be between {min} and {max}.";
     public static final String PASSWORD_CONFIRM_NOTBLANK = "Password confirmation must not be blank.";
     public static final String PASSWORD_CONFIRMATION = "Password confirmation does not match.";
 
@@ -56,7 +60,9 @@ public class RegisterRequest {
 
     @UniqueEmail(message = EMAIL_ALREADY_TAKEN)
     @Email(message = EMAIL_FORMAT)
+    @Size(max = EMAIL_SIZE_MAX, message = EMAIL_SIZE)
     @NotBlank(message = EMAIL_NOTBLANK)
+    @JsonIgnore
     @ToString.Exclude
     private String email;
 
@@ -66,10 +72,13 @@ public class RegisterRequest {
     @Pattern(regexp = "^.*[^\\w\\s].*$", message = PASSWORD_SPECIAL)
     @Size(min = PASSWORD_SIZE_MIN, max = PASSWORD_SIZE_MAX, message = PASSWORD_SIZE)
     @NotBlank(message = PASSWORD_NOTBLANK)
+    @JsonIgnore
     @ToString.Exclude
     private String password;
 
+    @Size(min = PASSWORD_SIZE_MIN, max = PASSWORD_SIZE_MAX, message = PASSWORD_CONFIRM_SIZE)
     @NotBlank(message = PASSWORD_CONFIRM_NOTBLANK)
+    @JsonIgnore
     @ToString.Exclude
     private String passwordConfirm;
 
